37fd61063c1ab437fa1c96ca0b4dbd5746c29d4c
Ehsan Eskandari <eskandarimashhadrizeie@hotmail.com>
2015-11-08 22:38:13 -0500

    Add nested view in edit page

--- app/products/productEditCtrl.js
+++ app/products/productEditCtrl.js
@@ -1,6 +1,3 @@
-/**
- * Created by Deb on 8/26/2014.
- */
 (function () {
     "use strict";
 
@@ -8,10 +5,11 @@
         .module("productManagement")
         .controller("ProductEditCtrl",
         ["product",
+            "$state",
             ProductEditCtrl]);
 
 
-    function ProductEditCtrl(product) {
+    function ProductEditCtrl(product, $state) {
         var vm = this;
 
         vm.product = product;
@@ -22,5 +20,38 @@
         else {
             vm.title = "New Product"
         }
+
+        vm.open = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            vm.opened = !vm.opened;
+        };
+
+        vm.submit = function () {
+            vm.product.$save(function (data) {
+                    toastr.success("Save Successful");
+                }
+            );
+        }
+
+        vm.cancel = function () {
+            $state.go('productList');
+        }
+
+        vm.addTags = function (tags) {
+            if (tags) {
+                var array = tags.split(',');
+                vm.product.tags = vm.product.tags ? vm.product.tags.concat(array) : array;
+                vm.newTags = "";
+            } else{
+                alert("Please enter one or more tags separated by commas");
+            }
+        }
+
+        vm.removeTag = function (idx) {
+            vm.product.tags.splice(idx, 1);
+        }
+
     }
 }());
--- app/products/productListCtrl.js
+++ app/products/productListCtrl.js
@@ -1,6 +1,3 @@
-/**
- * Created by Deb on 8/20/2014.
- */
 (function () {
     "use strict";
     angular
