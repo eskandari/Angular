72e1140796a6ee98ebd57daad0724b1b98bf76db
Ehsan Eskandari <eskandarimashhadrizeie@hotmail.com>
2015-11-12 22:30:31 -0500

    Added oData and filtering future

--- PersianRug.Client/app/products/productListCtrl.js
+++ PersianRug.Client/app/products/productListCtrl.js
@@ -8,8 +8,18 @@
 
     function ProductListCtrl(productResource) {
         var vm = this;
+        vm.searchCriteria = "PRI";
+        //1.Sample one
+        //productResource.query({search:vm.searchCriteria}, function(data) {
 
-        productResource.query(function(data) {
+        //2.second sample for more complex scenario
+        //productResource.query({
+        //     $filter: "contains(ProductCode, '" + searchCriteria + "')" +
+        //     " or  contains(ProductName, '" + vm.searchCriteria + "')" ,
+        //     $orderby: vm.sortProperty + " " + vm.sortDirection
+        //}, function(data) {
+
+        productResource.query({$filter:"contains(ProductCode,'PRI')",$skip:1, $top:3}, function (data) {
             vm.products = data;
         });
         vm.showImage = false;
--- PersianRug.WebAPI/Controllers/ProductsController.cs
+++ PersianRug.WebAPI/Controllers/ProductsController.cs
@@ -6,17 +6,29 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Web.OData;
 
 namespace APM.WebAPI.Controllers
 {
+    //[EnableCorsAttribute("http://54.68.71.220/PRS", "*", "*")]
     [EnableCorsAttribute("http://localhost:15209", "*", "*")]
     public class ProductsController : ApiController
     {
         // GET: api/Products
-        public IEnumerable<Product> Get()
+    //[EnableQuery(PageSize = 30, AllowedQueryOptions = AllowedQueryOptions.Skip)]
+        [EnableQuery(PageSize = 30)]
+        public IQueryable<Product> Get()
         {
             var productRepository = new ProductRepository();
-            return productRepository.Retrieve();
+            return productRepository.Retrieve().AsQueryable();
+        }
+
+        public IEnumerable<Product> Get(string search)
+        {
+            var productRepository = new ProductRepository();
+            var products = productRepository.Retrieve();
+            var result = products.Where(p => p.ProductCode.Contains(search));
+            return result;
         }
 
         // GET: api/Products/5
